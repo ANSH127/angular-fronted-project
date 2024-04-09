253fecb03a6741aeb472027794d05cbc8770e660
Ansh Agarwal <anshagrawal12348@gmail.com>
2024-04-10 01:08:00 +0530

    updating signup component

--- src/app/MyComponents/signup/signup.component.ts
+++ src/app/MyComponents/signup/signup.component.ts
@@ -1,12 +1,31 @@
 import { Component } from '@angular/core';
 
+import { NavcardComponent } from '../navcard/navcard.component';
+import { ProfilecardComponent } from '../profilecard/profilecard.component';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-signup',
   standalone: true,
-  imports: [],
+  imports: [NavcardComponent,ProfilecardComponent,CommonModule,FormsModule],
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.css'
 })
 export class SignupComponent {
+  name: string='';
+  email: string='';
+  password: string='';
+  constructor() { }
+
+  handleSignup() {
+    if(this.email == '' || this.password == '' || this.name == '') {
+      alert('Please fill all the fields');
+      return;
+    }
+
+    console.log(this.name);
+    console.log(this.email);
+    console.log(this.password);
+  }
 
 }
