47fcd1a9a2c5300a9a078fc4d8d87504546ecd17
Ansh Agarwal <anshagrawal12348@gmail.com>
2024-04-10 00:51:35 +0530

    updating file

--- src/app/MyComponents/confessioncard/confessioncard.component.ts
+++ src/app/MyComponents/confessioncard/confessioncard.component.ts
@@ -1,13 +1,21 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { NgHeroiconsModule } from "@dimaslz/ng-heroicons";
+import { NgHeroiconsModule } from '@dimaslz/ng-heroicons';
 
 @Component({
   selector: 'app-confessioncard',
   standalone: true,
-  imports: [NgHeroiconsModule],
+  imports: [NgHeroiconsModule,CommonModule],
   templateUrl: './confessioncard.component.html',
-  styleUrl: './confessioncard.component.css'
+  styleUrl: './confessioncard.component.css',
 })
 export class ConfessioncardComponent {
+  isLiked: Boolean = false;
+  constructor() {}
 
+  toggleLike() {
+    console.log('Like button clicked');
+    
+    this.isLiked = !this.isLiked;
+  }
 }
