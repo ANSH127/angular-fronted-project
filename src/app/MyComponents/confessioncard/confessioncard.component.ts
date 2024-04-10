32c23d7e93ac867bda120d0d8482b723180a9e08
Ansh Agarwal <anshagrawal12348@gmail.com>
2024-04-10 11:29:24 +0530

    adding comment box

--- src/app/MyComponents/confessioncard/confessioncard.component.ts
+++ src/app/MyComponents/confessioncard/confessioncard.component.ts
@@ -1,21 +1,34 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { NgHeroiconsModule } from '@dimaslz/ng-heroicons';
 
 @Component({
   selector: 'app-confessioncard',
   standalone: true,
-  imports: [NgHeroiconsModule,CommonModule],
+  imports: [NgHeroiconsModule,CommonModule,FormsModule],
   templateUrl: './confessioncard.component.html',
   styleUrl: './confessioncard.component.css',
 })
 export class ConfessioncardComponent {
   isLiked: Boolean = false;
+  showComment: Boolean = false;
+  comment: string = '';
   constructor() {}
 
   toggleLike() {
     console.log('Like button clicked');
     
     this.isLiked = !this.isLiked;
+  }
+
+  postComment() {
+    console.log('Comment posted: ', this.comment);
+    this.comment = '';
+  }
+
+  toggleComment() {
+    console.log('Comment button clicked');
+    this.showComment = !this.showComment;
   }
 }
