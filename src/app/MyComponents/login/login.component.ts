253012b7c5e412e4091930fef7920fa319f4ebd1
Ansh Agarwal <anshagrawal12348@gmail.com>
2024-04-11 10:11:09 +0530

    updating handleLogin function

--- src/app/MyComponents/login/login.component.ts
+++ src/app/MyComponents/login/login.component.ts
@@ -10,22 +10,50 @@ import { TrendingcardComponent } from '../trendingcard/trendingcard.component';
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [NavcardComponent,ProfilecardComponent,CommonModule,FormsModule,RouterLink,SearchcardComponent,TrendingcardComponent],
+  imports: [
+    NavcardComponent,
+    ProfilecardComponent,
+    CommonModule,
+    FormsModule,
+    RouterLink,
+    SearchcardComponent,
+    TrendingcardComponent,
+  ],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrl: './login.component.css',
 })
 export class LoginComponent {
-  email: string='';
-  password: string='';
-  constructor() { }
+  email: string = '';
+  password: string = '';
+  constructor() {}
 
-  handleLogin() {
-    if(this.email == '' || this.password == '') {
+  handleLogin = async () => {
+    if (this.email == '' || this.password == '') {
       alert('Please fill all the fields');
       return;
     }
+    try {
+      const data = { email: this.email, password: this.password };
+      const response = await fetch('http://localhost:4000/api/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+      });
+      
+      const data2 = await response.json();
+      // console.log(data2);
+      if (data2.message) {
+        alert(data2.message);
+      } else {
+        // set the token in local storage
+        localStorage.setItem('token', data2.token);
 
-    console.log(this.email);
-    console.log(this.password);
-  }
+        alert('Login successful');
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
 }
