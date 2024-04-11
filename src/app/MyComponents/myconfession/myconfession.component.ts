f26143f1d59b0007df1fde76af981aab10509449
Ansh Agarwal <anshagrawal12348@gmail.com>
2024-04-11 11:33:37 +0530

    adding myconfession function

--- src/app/MyComponents/myconfession/myconfession.component.ts
+++ src/app/MyComponents/myconfession/myconfession.component.ts
@@ -9,10 +9,44 @@ import { TrendingcardComponent } from '../trendingcard/trendingcard.component';
 @Component({
   selector: 'app-myconfession',
   standalone: true,
-  imports: [NavcardComponent,ProfilecardComponent,ConfessioncardComponent,CommonModule,SearchcardComponent,TrendingcardComponent],
+  imports: [
+    NavcardComponent,
+    ProfilecardComponent,
+    ConfessioncardComponent,
+    CommonModule,
+    SearchcardComponent,
+    TrendingcardComponent,
+  ],
   templateUrl: './myconfession.component.html',
-  styleUrl: './myconfession.component.css'
+  styleUrl: './myconfession.component.css',
 })
 export class MyconfessionComponent {
+  confessions: any = [];
+  isloding: boolean = false;
+  constructor() {}
 
+  fetchConfession = async () => {
+    try {
+      this.isloding = true;
+      const response = await fetch('http://localhost:4000/api/userconfessions', {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${localStorage.getItem('token')}`,
+        },
+      });
+
+      const data = await response.json();
+      this.confessions = data;
+      // console.log(data);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      this.isloding = false;
+    }
+  };
+
+  ngOnInit() {
+    this.fetchConfession();
+  }
 }
