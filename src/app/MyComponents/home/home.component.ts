4eaedcccae2c976eb5838c0ca40ac5caef492154
Ansh Agarwal <anshagrawal12348@gmail.com>
2024-04-11 10:26:02 +0530

    adding getConfessions function

--- src/app/MyComponents/home/home.component.ts
+++ src/app/MyComponents/home/home.component.ts
@@ -8,10 +8,42 @@ import { TrendingcardComponent } from '../trendingcard/trendingcard.component';
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [NavcardComponent,ProfilecardComponent,ConfessioncardComponent,CommonModule,SearchcardComponent,TrendingcardComponent],
+  imports: [
+    NavcardComponent,
+    ProfilecardComponent,
+    ConfessioncardComponent,
+    CommonModule,
+    SearchcardComponent,
+    TrendingcardComponent,
+  ],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
 export class HomeComponent {
+  confessions: any = [];
 
+  constructor() {}
+
+  ngOnInit() {
+    // fetch the confessions
+    this.getConfessions();
+  }
+
+  getConfessions = async () => {
+    try {
+      const response = await fetch('http://localhost:4000/api/confessions', {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${localStorage.getItem('token')}`,
+        },
+      });
+
+      const data = await response.json();
+      this.confessions = data;
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 }
