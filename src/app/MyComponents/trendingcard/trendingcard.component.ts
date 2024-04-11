43fe800e6ff1da954eda381b8b1183331239430f
Ansh Agarwal <anshagrawal12348@gmail.com>
2024-04-11 14:25:18 +0530

    fetching Trending confessions

--- src/app/MyComponents/trendingcard/trendingcard.component.ts
+++ src/app/MyComponents/trendingcard/trendingcard.component.ts
@@ -6,8 +6,38 @@ import { CommonModule } from '@angular/common';
   standalone: true,
   imports: [CommonModule],
   templateUrl: './trendingcard.component.html',
-  styleUrl: './trendingcard.component.css'
+  styleUrl: './trendingcard.component.css',
 })
 export class TrendingcardComponent {
+  confessions: any = [];
+  fetchTrending = async () => {
+    try {
+      const response = await fetch(
+        'http://localhost:4000/api/trendingconfessions',
+        {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${localStorage.getItem('token')}`,
+          },
+        }
+      );
 
+      const data = await response.json();
+      console.log('Trending data: ', data);
+
+      if (data.error) {
+        alert(data.error);
+        return;
+      }
+
+      this.confessions = data
+    } catch (error) {
+      console.error('Error fetching trending: ', error);
+    }
+  };
+
+  ngOnInit() {
+    this.fetchTrending();
+  }
 }
